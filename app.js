2f1cbb74cbe89625a1aeacd65e5b57ece789e6b6
asqarovilhomjon <asqarovilhomjon045@gmail.com>
2025-01-05 11:55:34 +0500

    typescript

---
+++ app.js
@@ -0,0 +1,48 @@
+// // 1-task
+// function evenSum(n: number): number {
+//     let sum = 0;
+//     for (let i = 0; i <= n; i += 2) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
+//   console.log(evenSum(7));
+//   // 2-task
+//   function boolToNum(value: boolean): number {
+//     return value ? 1 : 0;
+//   }
+//   console.log(boolToNum(true));
+//   console.log(boolToNum(false));
+//   // 3-task
+//   function firstTwo(name: string): string {
+//     return name.slice(0, 2);
+//   }
+//   console.log(firstTwo("Laylo"));
+//   // 4-task
+//   function addTwoRound(num: number): number {
+//     return Math.round(num + 2);
+//   }
+//   console.log(addTwoRound(5.3));
+//   // 5-task
+//   function incToStr(num: number): string {
+//     return (num + 1).toString();
+//   }
+//   console.log(incToStr(4));
+//   // 6-task
+//   function printChars(str: string): void {
+//     for (const char of str) {
+//       console.log(char);
+//     }
+//   }
+//   printChars("salom");
+//   // 8-task
+//   function isOne(num: number): boolean {
+//     return num === 1;
+//   }
+//   console.log(isOne(1));
+//   console.log(isOne(0));
+//   // 9-task
+//   function intro(name: string, nationality: string): string {
+//     return `My name is ${name}. I'm ${nationality}`;
+//   }
+//   console.log(intro("Laylo", "Uzbek"));
